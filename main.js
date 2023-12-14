6c4259ea2580aa4591ee133e42300470b0a382ec
Alejandro Pinedo <danielpinedo24@gmail.com>
2023-12-14 13:51:13 -0600

    Actualizado a nodos y sustitucion de algunas funciones

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
+/// Dedicar la primera seccion solamente a la creacion de alumnos y sus atributos
+
 class Alumno{ 
-    constructor(nombre, apellido, edad, materiasInscritas, calificaciones) {
+    constructor(nombre, apellido, edad, materiasInscritas, telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
-        this.materiasInscritas = materiasInscritas;
-        this.calificaciones = calificaciones; 
+        this.materiasInscritas = [];
+        this.telefono = telefono;
+        this.calificaciones = {};  /// ya que son valores, es posible hacerlo un array directamente? ///
+        this.siguienteAlumno = null; 
     }
 
-    asignarCalificacion(materia, calificacion){
-        this.calificaciones[materia] = calificacion;
+    asignarNota(materia, nota){
+        this.calificaciones[materia] = nota;
+        return alert('Calificación agregada exitosamente'); 
+        console.log('Nota agregada'); 
     }
 
-/// La Const de numero de materias es necesario 
     promedioCalificaciones(){
-        const numMaterias = object.keys(this.calificaciones).lenght; 
-        if (numMaterias === 0) {
+        const totalMaterias = Object.keys(this.calificaciones).lenght; 
+        if (totalMaterias === 0) 
+            console.log('Sin materias agregadas');
+ { 
             return 0;
         }
-
-        const sum= Object.values(this.calificaciones).reduce((total, calificacion) => total + calificacion, 0);
+        const sum= Object.values(this.calificaciones).reduce((total, nota) => total + nota, 0);
         return sum / numMaterias; 
     }
 
-    static agregarAlumno(nombre, apellido, edad, materiasInscritas, calificaciones){
-        const nuevoAlumno = new Alumno(nombre, apellido, edad, materiasInscritas, calificaciones);
+}
+/// Dedicar la segunda seccion a la creacion de metodos, a los grupos y las listas.
+
+class Grupo{ 
+    constructor(nombredelgrupo){ 
+        this.nombredelgrupo = nombredelgrupo;
+        this.primerAlumno = null; /// Esto seria el primer nodo? /// 
     }
+
+    agregarAlumno(alumno) { 
+        if (!this.primerAlumno) { 
+            this.primerAlumno = alumno; 
+      console.log('Primer alumno inscrito a este grupo'); }   
+        
+      else{ 
+            let ultimoAlumno = this.primerAlumno; 
+            while (ultimoAlumno.siguienteAlumno) {
+                ultimoAlumno = ultimoAlumno.siguienteAlumno;
+            }
+            ultimoAlumno.siguienteAlumno = alumno; 
+        }
+    }
+
 }
