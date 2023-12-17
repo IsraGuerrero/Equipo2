23c7539896f0ce65eda6be47f0c34747f8d6df67
jesusmacq <jesus.quezada99@gmail.com>
2023-12-17 00:09:36 -0600

    Hice la class ListaLigada, para crear la lista ligada, el metodo push lo agregue pero todavia no lo pruebo. aparte cree abajo una const lista para hacer la prueba y si me sube los valores al alumno

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 /// Dedicar la primera seccion solamente a la creacion de alumnos y sus atributos
 
 class Alumno{ 
-    constructor(nombre, apellido, edad, materiasInscritas, telefono) {
+    constructor(nombre, apellido, edad, materiasInscritas, telefono, calificaciones) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
-        this.materiasInscritas = [];
+        this.materiasInscritas = [""];
         this.telefono = telefono;
-        this.calificaciones = {};  /// ya que son valores, es posible hacerlo un array directamente? ///
+        this.calificaciones = [];  /// ya que son valores, es posible hacerlo un array directamente? ///
         this.siguienteAlumno = null; 
     }
+}
+class ListaLigada{
+        constructor(nombre, apellido, edad, materiasInscritas, telefono, calificaciones) {
+            const newAlumno = new Alumno(nombre, apellido, edad, materiasInscritas, telefono, calificaciones);
+            this.head = newAlumno;
+            this.tale = newAlumno;
+            this.lenght = 1
+        }
 
+        push(nombre, apellido, edad, materiasInscritas, telefono, calificaciones){
+            const newAlumno = new Alumno(nombre, apellido, edad, materiasInscritas, telefono, calificaciones);
+            if(!this.head){ //si la cola esta vacia
+                this.head = newAlumno;
+                this.tale = newAlumno;
+            } else{
+                this.tale.next = newAlumno;
+                this.tale = newAlumno;
+            }
+            this.length++;
+            return this;
+        }
+}
+
+const lista = new ListaLigada("Marco", "Quezada", 23, ["Igles", "Quimica"], 8721171115, [[9, 8, 8, 9], [10, 9, 9, 9]]);
+console.log(lista);
+
+/*
     asignarNota(materia, nota){
         this.calificaciones[materia] = nota;
         return alert('Calificación agregada exitosamente'); 
@@ -28,7 +54,7 @@ class Alumno{
         return sum / numMaterias; 
     }
 
-}
+
 /// Dedicar la segunda seccion a la creacion de metodos, a los grupos y las listas.
 
 class Grupo{ 
@@ -52,3 +78,4 @@ class Grupo{
     }
 
 }
+*/
